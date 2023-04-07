a29d128754efea83fb960cc358f43a6a557fef1e
wanwu <xoxsky@qq.com>
2023-04-07 14:56:19 +0800

    0ne2

---
+++ src/Demo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Apart\Wanwu;
+
+class Demo {
+
+	public static function sayHello() {
+
+		return 'Good morning';
+
+	}
+
+}
+// https://blog.csdn.net/weixin_39781323/article/details/115946920
